29efe758a2a966c7c25e508ae7d22fa6dae39e66
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2025-05-21 10:36:08 +0530

    Django

--- learning1/app1/admin.py
+++ learning1/app1/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import AppVarity
+from .models import AppVarity, AppReview, AppVerification, Store
 
 # Register your models here.
-admin.site.register(AppVarity)
+
+class AppRevieInLine(admin.TabularInline):
+    model = AppReview
+    extra = 2
+
+class AppVarityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [AppRevieInLine]
+
+class StoreAdmin(admin.ModelAdmin): 
+    list_display = ('name', 'location')
+    filter_horizontal = ('app_varities', )
+
+class AppVerificationAdmin(admin.ModelAdmin):
+    list_display = ('app1', 'certificate_number')
+
+# class AppBuyingAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'buying', 'issue_date')
+
+admin.site.register(AppVarity, AppVarityAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(AppVerification, AppVerificationAdmin)
+# admin.site.register(AppBuying, AppBuyingAdmin)
--- learning1/app1/models.py
+++ learning1/app1/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class AppVarity(models.Model):
@@ -18,3 +19,46 @@ class AppVarity(models.Model):
 
     def __str__(self):
         return self.name
+    
+# One to Many
+class AppReview(models.Model):
+    app1 = models.ForeignKey(AppVarity, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.app1.name}'
+
+
+# Many to Many
+class Store(models.Model):
+    name = models.CharField(max_length=400)
+    location = models.CharField(max_length=400)
+    app_varities = models.ManyToManyField(AppVarity, related_name='stores')
+
+    def __str__(self):
+        return self.name
+
+
+# One to One 
+class AppVerification(models.Model):
+    app1 = models.OneToOneField(AppVarity, on_delete=models.CASCADE, related_name='verify')
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    valid_date = models.DateTimeField()
+
+    def __str__(self):
+        return f'Verification for {self.name.app1}'
+    
+
+# class AppBuying(models.Model):
+#     name = models.CharField(max_length=40)
+#     app1 = models.OneToOneField(AppVarity, on_delete=models.CASCADE, related_name='verify')
+#     buying = models.CharField(max_length=45, default='0')
+#     issue_date = models.DateTimeField(default=timezone.now)
+
+#     def __str__(self):
+#         return f'You buy this app {self.name.app1}'
+    
